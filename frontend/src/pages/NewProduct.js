fdad391fb47735921cf87b97d499b7c85ed39ebc
VictorRibeiroH <contato.victoribeiro@gmail.com>
2022-07-22 10:34:48 -0300

    fixando erros

---
+++ frontend/src/pages/NewProduct.js
@@ -0,0 +1,160 @@
+import React, { useState } from "react";
+import { Alert, Col, Container, Form, Row, Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import { useCreateProductMutation } from "../services/appApi";
+import axios from "../axios";
+import "./NewProduct.css";
+
+function NewProduct() {
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState("");
+  const [category, setCategory] = useState("");
+  const [images, setImages] = useState([]);
+  const [imgToRemove, setImgToRemove] = useState(null);
+  const navigate = useNavigate();
+  const [createProduct, { isError, error, isLoading, isSuccess }] =
+    useCreateProductMutation();
+
+  function handleRemoveImg(imgObj) {
+    setImgToRemove(imgObj.public_id);
+    axios
+      .delete(`/images/${imgObj.public_id}/`)
+      .then((res) => {
+        setImgToRemove(null);
+        setImages((prev) =>
+          prev.filter((img) => img.public_id !== imgObj.public_id)
+        );
+      })
+      .catch((e) => console.log(e));
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!name || !description || !price || !category || !images.length) {
+      return alert("Por favor preencha todos os campos");
+    }
+    createProduct({ name, description, price, category, images }).then(
+      ({ data }) => {
+        if (data.length > 0) {
+          setTimeout(() => {
+            navigate("/");
+          }, 1500);
+        }
+      }
+    );
+  }
+
+  function showWidget() {
+    const widget = window.cloudinary.createUploadWidget(
+      {
+        cloudName: "dvmdsvolm",
+        uploadPreset: "qlw0cv1z",
+      },
+      (error, result) => {
+        if (!error && result.event === "success") {
+          setImages((prev) => [
+            ...prev,
+            { url: result.info.url, public_id: result.info.public_id },
+          ]);
+        }
+      }
+    );
+    widget.open();
+  }
+
+  return (
+    <Container>
+      <Row>
+        <Col md={6} className="new-product__form--container">
+          <Form style={{ width: "100%" }} onSubmit={handleSubmit}>
+            <h1 className="mt-4">Criar produto</h1>
+            {isSuccess && (
+              <Alert variant="success">Produto criado com sucesso</Alert>
+            )}
+            {isError && <Alert variant="danger">{error.data}</Alert>}
+            <Form.Group className="mb-3">
+              <Form.Label>Nome do produto</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Adicione o nome do produto"
+                value={name}
+                required
+                onChange={(e) => setName(e.target.value)}
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+              <Form.Label>Descrição do Produto</Form.Label>
+              <Form.Control
+                as="textarea"
+                placeholder="Descrição do produto"
+                style={{ height: "100px" }}
+                value={description}
+                required
+                onChange={(e) => setDescription(e.target.value)}
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+              <Form.Label>Preço($)</Form.Label>
+              <Form.Control
+                type="number"
+                placeholder="Preço (R$)"
+                value={price}
+                required
+                onChange={(e) => setPrice(e.target.value)}
+              />
+            </Form.Group>
+
+            <Form.Group
+              className="mb-3"
+              onChange={(e) => setCategory(e.target.value)}
+            >
+              <Form.Label>Categoria</Form.Label>
+              <Form.Select>
+                <option disabled selected>
+                  -- Select One --
+                </option>
+                <option value="technology">tecnologia</option>
+                <option value="tablets">tablets</option>
+                <option value="pops">pops</option>
+                <option value="actionfig">action figure</option>
+                <option value="phones">phones</option>
+                <option value="laptops">laptops</option>
+              </Form.Select>
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+              <Button type="button" onClick={showWidget}>
+                Adicionar imagens
+              </Button>
+              <div className="images-preview-container">
+                {images.map((image) => (
+                  <div className="image-preview">
+                    <img src={image.url} />
+                    {imgToRemove != image.public_id && (
+                      <i
+                        className="fa fa-times-circle"
+                        onClick={() => handleRemoveImg(image)}
+                      ></i>
+                    )}
+                  </div>
+                ))}
+              </div>
+            </Form.Group>
+
+            <Form.Group>
+              <Button type="submit" disabled={isLoading || isSuccess}>
+                Criar produto!
+              </Button>
+            </Form.Group>
+          </Form>
+        </Col>
+        <Col md={6} className="new-product__image--container"></Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default NewProduct;
